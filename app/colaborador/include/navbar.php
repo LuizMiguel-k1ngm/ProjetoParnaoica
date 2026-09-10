6084ed76e3e5e0477087b3ecfb3ff6b2ab58d49a
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-09-10 10:38:44 -0300

    add:> navbar

---
+++ app/colaborador/include/navbar.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+
+  <div class="container-fluid">
+
+    <a class="navbar-brand fw-bold" href="../colaborador/painel.php">
+      MENU ADM
+    </a>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarAdmin"
+      aria-controls="navbarAdmin"
+      aria-expanded="false"
+      aria-label="Abrir menu">
+
+      <span class="navbar-toggler-icon"></span>
+
+    </button>
+
+
+    <div
+      class="collapse navbar-collapse"
+      id="navbarAdmin">
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+      </ul>
+
+      <a
+        class="btn btn-outline-danger"
+        href="../index.php">
+        <i class="bi bi-box-arrow-right"></i>
+        Sair
+      </a>
+
+    </div>
+
+  </div>
+
+</nav>
